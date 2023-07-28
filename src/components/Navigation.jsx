a62dcacaa0a727858d9a056244e520c4c49daa16
JenskeM <jenske_meijs16@hotmail.com>
2023-07-28 11:26:21 +0200

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Grid, GridItem, Text } from "@chakra-ui/react";
 
 export const Navigation = () => {
   return (
-    <nav>
-      <ul>
-        <li>
+    <Grid
+      bg="orange.400"
+      templateColumns="repeat(8, 1fr)"
+      templateRows={1}
+      marginBottom={10}
+      height={8}
+      fontSize="xl"
+      color="white"
+    >
+      <GridItem colSpan={6} fontWeight={"bold"} paddingLeft={2}>
+        <Text> My Event App</Text>
+      </GridItem>
+      <GridItem colSpan={2}>
+        <Grid templateColumns={"1fr 1fr"} templateRows={1} gap={1}>
           <Link to="/">Events</Link>
-        </li>
-        <li>
           <Link to="/event/1">Event</Link>
-        </li>
-      </ul>
-    </nav>
+        </Grid>
+      </GridItem>
+    </Grid>
   );
 };
