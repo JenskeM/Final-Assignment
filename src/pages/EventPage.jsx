871a3e474f54d6864ade8aa6d2ad5b2a36f625c4
JenskeM <jenske_meijs16@hotmail.com>
2023-08-06 19:16:34 +0200

    Update EventPage.jsx

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -1,20 +1,26 @@
 import React from "react";
+import { useLoaderData } from "react-router-dom";
 import { Heading } from "@chakra-ui/react";
 
 export const loader = async ({ params }) => {
-  const events = await fetch(`http://localhost:3000/events/${params.id}`);
+  const event = await fetch(`http://localhost:3000/event/6}`);
   const categories = await fetch(`http://localhost:3000/categories`);
   const users = await fetch(`http://localhost:3000/users`);
 
   return {
-    events: await events.json(),
+    event: await event.json(),
     categories: await categories.json(),
     users: await users.json(),
   };
 };
 
 export const EventPage = () => {
-  const { events, categories, users } = useLoaderData();
+  const { event, categories, users } = useLoaderData();
 
-  return <Heading>Event</Heading>;
+  return (
+    <>
+      <Heading>Test</Heading>
+      {event && console.log(event)}
+    </>
+  );
 };
