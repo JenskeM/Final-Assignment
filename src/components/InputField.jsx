a9d8fe5b87c50099e87e06c3024a40e7c1e90354
JenskeM <jenske_meijs16@hotmail.com>
2023-08-23 21:01:12 +0200

    Update InputField.jsx

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -3,7 +3,7 @@ import { Grid, FormLabel, Input } from "@chakra-ui/react";
 export const InputField = ({ labelText, nameText, screenSize }) => {
   return (
     <Grid
-      gridTemplateColumns={screenSize.width <= 360 ? 300 : "110px 300px"}
+      gridTemplateColumns={screenSize.width <= 700 ? 300 : "110px 300px"}
       alignItems={"center"}
     >
       <FormLabel>{labelText}</FormLabel>
