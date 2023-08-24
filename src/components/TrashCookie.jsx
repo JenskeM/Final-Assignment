fbce5f027d490cc951cd0283149aecb9796892c6
JenskeM <jenske_meijs16@hotmail.com>
2023-08-24 12:19:24 +0200

    Update TrashCookie.jsx

--- src/components/TrashCookie.jsx
+++ src/components/TrashCookie.jsx
@@ -2,8 +2,18 @@ import { Box, Image, Center } from "@chakra-ui/react";
 import "./trashCookie.css";
 
 export const TrashCookie = ({ screenSize }) => {
+  const overlayStyle = {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: "rgba(180, 195, 157, 0.73)",
+    zIndex: 1000,
+  };
+
   return (
-    <Box className="Nocookie-container">
+    <Box className="Nocookie-container" style={overlayStyle}>
       <div
         className="NoCookie"
         style={{
